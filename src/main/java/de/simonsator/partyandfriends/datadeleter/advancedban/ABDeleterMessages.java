5fb84aa45cd04a6b1d57acbd5dc98eb659cab832
J.T.McQuigg <J.T.McQuigg12@gmail.com>
2022-12-11 00:30:39 -0500

    Use non deprecated LanguageConfiguration

--- src/main/java/de/simonsator/partyandfriends/datadeleter/advancedban/ABDeleterMessages.java
+++ src/main/java/de/simonsator/partyandfriends/datadeleter/advancedban/ABDeleterMessages.java
@@ -1,15 +1,15 @@
 package de.simonsator.partyandfriends.datadeleter.advancedban;
 
+import de.simonsator.partyandfriends.api.PAFPluginBase;
 import de.simonsator.partyandfriends.utilities.Language;
 import de.simonsator.partyandfriends.utilities.LanguageConfiguration;
-import net.md_5.bungee.api.plugin.Plugin;
 
 import java.io.File;
 import java.io.IOException;
 
 public class ABDeleterMessages extends LanguageConfiguration {
 
-	protected ABDeleterMessages(File file, Plugin pPlugin) throws IOException {
+	protected ABDeleterMessages(File file, PAFPluginBase pPlugin) throws IOException {
 		super(Language.ENGLISH, file, pPlugin);
 		copyFromJar();
 		readFile();
